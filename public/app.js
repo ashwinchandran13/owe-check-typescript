d29369311c5790e3b31ddb71a33082a369c0bf55
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-07-10 14:15:50 +0530

    classes basics

--- public/app.js
+++ public/app.js
@@ -1,10 +1,24 @@
 "use strict";
-// const anchor = document.querySelector('a')!;
-// console.log(anchor.href);
-// if (anchor) {
-//   console.log(anchor);
-// }
-// const form = document.querySelector('form')!;
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on mario website', 200);
+var invTwo = new Invoice('Luigi', 'work on luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invOne.client = 'yoshi';
+// invTwo.amount = 400;
+console.log(invOne, invTwo);
 var form = document.querySelector('.new-item-form');
 // console.log(form.children);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,35 @@
-// const anchor = document.querySelector('a')!;
+// classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
-// console.log(anchor.href);
+    constructor(c: string, d: string, a: number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
 
-// if (anchor) {
-//   console.log(anchor);
-// }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
 
-// const form = document.querySelector('form')!;
+const invOne = new Invoice('mario', 'work on mario website', 200);
+const invTwo = new Invoice('Luigi', 'work on luigi website', 300);
+
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+// invOne.client = 'yoshi';
+// invTwo.amount = 400;
+
+console.log(invOne, invTwo);
+
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
 
