1e72a694155e3aaf57c86e329ea77e328ca98a27
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-07-10 18:05:42 +0530

    rendered lists into html

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,9 @@
 import{ Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-
-// const invOne = new Invoice('mario', 'work on mario website', 200);
-// const invTwo = new Invoice('Luigi', 'work on luigi website', 300);
-
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach(inv => {
-//     console.log(inv.client, inv.amount, inv.format());
-// })
-
-// invOne.client = 'yoshi';
-// invTwo.amount = 400;
-
-// console.log(invOne, invTwo);
-
-
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
@@ -43,7 +11,11 @@ const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form .addEventListener('submit', (e: Event) => {
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
     let doc: HasFormatter;
@@ -52,7 +24,7 @@ form .addEventListener('submit', (e: Event) => {
     } else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
     }
-
-    console.log(doc);
     
-})
+    list.render(doc, type.value, 'end');
+
+});
