48569a903624eabc490ea75ac61350cfb4e1b829
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-07-10 13:48:13 +0530

    Dom manipulation

---
+++ public/app.js
@@ -0,0 +1,18 @@
+"use strict";
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href);
+// if (anchor) {
+//   console.log(anchor);
+// }
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+// console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
